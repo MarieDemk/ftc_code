6b7c56570261c1f309760692d76af5e1a773d26f
AditSwami <swamiaditswami@gmail.com>
2024-11-22 23:08:39 -0600

    new servo code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -2,7 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp (name = "Drive")
 public class Drive extends OpMode {
@@ -12,11 +17,12 @@ public class Drive extends OpMode {
     @Override
     public void init() {
         driver.init(hardwareMap);
-        servo.init("qw",hardwareMap,0.5);
+        servo.init("armServo",hardwareMap, telemetry, 1);
     }
 
     @Override
     public void loop() {
         driver.moveRobot(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+        servo.openClaw(gamepad1.left_bumper,0.5);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servo_settings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servo_settings.java
@@ -2,24 +2,32 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class Servo_settings {
     public Servo myservo;
     public boolean lasButtonPres = false;
     public boolean isClawOpen = false;
     public double startingServoPos;
+    private Telemetry telemetry; // Instance-specific telemetry
 
-    public void init(String servoName, HardwareMap hwMap, double startingPos){
+    public void init(String servoName, HardwareMap hwMap, Telemetry telemetry, double startingPos) {
+        this.telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         myservo = hwMap.get(Servo.class, servoName);
         startingServoPos = startingPos;
         myservo.setPosition(startingServoPos);
     }
 
-    public void openClaw(boolean buttonPressed, double openPos){
-        if(buttonPressed && !lasButtonPres){
+    public void openClaw(boolean buttonPressed, double openPos) {
+        if (buttonPressed && !lasButtonPres) {
             isClawOpen = !isClawOpen;
             myservo.setPosition(isClawOpen ? openPos : startingServoPos);
+            telemetry.addData("buttonPressed", buttonPressed);
+            telemetry.update();
         }
         lasButtonPres = buttonPressed;
     }
-
 }
